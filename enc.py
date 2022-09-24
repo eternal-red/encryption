fba3bd1fe795877d675a32c9bcf7b6e43b417453
Yuri Xu <80001670+eternal-red@users.noreply.github.com>
2022-09-24 12:22:13 -0500

    Add files via upload

---
+++ enc.py
@@ -0,0 +1,58 @@
+import math
+import decimal
+from decimal import *
+getcontext().prec=200
+
+def cos(x):
+    getcontext().prec += 2
+    i, lasts, s, fact, num, sign = 0, 0, 1, 1, 1, 1
+    while s != lasts: #run until nth term added to s is so insignficant s==lasts
+        lasts = s
+        i += 2
+        fact *= i * (i-1)
+        num *= x * x
+        sign *= -1
+        s += num / fact * sign
+        #print(type(s))
+    getcontext().prec -= 2
+    return +s  #performs an operation to change s to new precision
+
+def acos(x):
+    getcontext().prec += 2
+    k, lasts = 0, 0
+    s, fact, k_exclam, half_poc = 1, 1, 1, 1
+    num= x
+    while s!= lasts:
+        k+=1
+        lasts = s
+        half_poc*= (2*k)*(2*k-1)/2
+        k_exclam*=k
+        num*= x**2
+        fact=k_exclam+2*k*k_exclam
+        s += num*Decimal(half_poc)/Decimal(fact)
+    getcontext().prec -= 2
+    return +s
+
+def enc(x,y):
+    for i in range(100):
+        y=Decimal(x)+Decimal(cos(y))
+    return y
+
+def dec(x,z):
+    for i in range(100): 
+        z=Decimal(math.pi/2)-acos(Decimal(z)-Decimal(x))
+         
+    return z/4
+
+def test(x,y):
+    getcontext().prec=50
+    z=Decimal(x)/Decimal(y) #be sure to do this not (Decimal(x/y)
+    return z
+
+
+#print(enc(1.13598729, 1.29384))  #1.1... is ideal
+#print(dec(1.43598729, 1.3827853845447892783768614148704878803618413235839504283525029945052193226467966208523589882421354895049061520427437847541016493692372719634530040484699471316565600730160698601549434455002560720781340))
+#print(math.acos(-0.5))
+
+print(math.acos(-0.05))
+print(acos(Decimal(-0.05)))
